6d96cb2bb39f185dc1ea8d6f0887ce913551660e
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-11 18:50:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name){
 }
 
 function holidayCountdown(holiday, days){
-  return "it\'s " + days +" until " + holiday;
+  return "It\'s " + days +" until " + holiday;
 }
