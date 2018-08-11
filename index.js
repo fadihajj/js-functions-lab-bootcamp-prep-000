063bf5c485213178b17f1fb27f85775446ee2224
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-11 18:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function haapyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return 'Happy holidays, ' + name + '.';
+  return 'Happy holidays, ' + name + '!';
 }
 
 function happyHolidayTo(holiday, name){
